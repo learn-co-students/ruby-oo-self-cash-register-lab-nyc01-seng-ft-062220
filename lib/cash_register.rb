a25e79a96491dfcb5aa280896f76af4cf87e902f
“jzhouerm” <jzhouerm@hotmail.comgit config --global user.name jzhouermgit config --global user.email jzhouerm@hotmail.com>
2020-06-25 12:10:55 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,36 @@
 
+class CashRegister
+
+    attr_accessor :discount, :total, :items, :last_transaction_amount
+
+    def initialize(discount=0)
+        @total = 0
+        @discount = discount
+        @items =[]
+    end
+
+   def add_item(title, price, quantity=1)
+    i = 0 
+    @last_transaction_amount = @total
+    while i < quantity
+            @total += (price * quantity)
+            @items.push(title)
+            i+= 1
+        end
+        
+   end
+
+   def apply_discount
+        self.total = @total -= (@total * discount / 100.to_f)
+        if discount == 0
+            "There is no discount to apply."
+        elsif discount != 0
+            "After the discount, the total comes to $#{self.total.to_i}."
+        end
+
+   end
+
+   def void_last_transaction
+    @total = @total - @last_transaction_amount
+    end
+end
